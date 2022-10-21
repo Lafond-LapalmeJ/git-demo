72ce635b1e6bd1b426bf828beff4c46b9f3f0503
Joel Lafond-Lapalme <joel.lafond-lapalme@agr.gc.ca>
2022-10-21 09:13:58 -0400

    Add quarto file and delete rsync script

--- rsync_git-demo.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-rsync -avu -delete "../git_resources/git_introduction_files/" "git_introduction_files"
-
-rsync -avu -delete "../git_resources/git_introduction.html" "git_introduction.html"
-
-rsync -avu -delete "../git_resources/images/" "images"
